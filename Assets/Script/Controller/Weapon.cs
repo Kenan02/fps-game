e1d46578756fe1fcc30dba100f17b7683b056021
Kenan02 <61836371+Kenan02@users.noreply.github.com>
2021-03-11 15:19:47 +0100

    UPDATERING

--- Assets/Script/Controller/Weapon.cs
+++ Assets/Script/Controller/Weapon.cs
@@ -3,81 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using kenan.daniel.spel;
 
-    public class Weapon : MonoBehaviour
+public class Weapon : MonoBehaviour
+{
+    public Gun[] loadout;
+    public Transform weaponParent;
+    private int currentIndex;
+
+
+    //bullet holes
+    public GameObject bulletHolePrefab;
+    public LayerMask canBeShot;
+    private GameObject currentWeapon;
+
+
+    //Ammo/shooting
+    public float Damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+
+
+
+    void Update()
     {
-        public Gun[] loadout;
-        public Transform weaponParent;
-        private int currentIndex;
-        public ParticleSystem muzzleFlash;
 
-        //bullet holes
-        public GameObject bulletHolePrefab;
-        public LayerMask canBeShot;
-        private GameObject currentWeapon;
+        //Equip Weapons
+        if (Input.GetKeyDown(KeyCode.Alpha1)) Equip(0);
 
-        //Ammo/shooting
-        public float dmg = 10f;
-        public float range = 100f;
-        public int maxAmmo = 10;
-        public Camera fpsCam;
-        private int currentAmmo = -1;
-        public float reloadTime = 1f;
-       
-        void Update()
+        if (currentWeapon != null)
         {
-            if(currentAmmo <= 0)
+            if (Input.GetMouseButtonDown(0)) 
             {
-                Reload();
-                return;
-            }
-
-            //Equip Weapons
-            if (Input.GetKeyDown(KeyCode.Alpha1)) Equip(0);
-            if (Input.GetKeyDown(KeyCode.Alpha2)) Equip(1);
-            if (Input.GetKeyDown(KeyCode.Alpha3)) Equip(2);
-            if (Input.GetKeyDown(KeyCode.Alpha4)) Equip(3);
-
-            if (currentWeapon != null)
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Shoot();
-                }
-
+                Shoot();
             }
         }
-
-        void Reload()
-        {
-            Debug.Log("Realoding...");
-            currentAmmo = maxAmmo;
-        }
-
-
-        void Equip(int p_ind)
-        {
-            if (currentWeapon != null) Destroy(currentWeapon);
-
-            currentIndex = p_ind;
-
-            GameObject t_newEquipment = Instantiate(loadout[p_ind].prefab, weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
-            t_newEquipment.transform.localEulerAngles = Vector3.zero;
-            t_newEquipment.transform.localPosition = Vector3.zero;
-
-            currentWeapon = t_newEquipment;
-        }
-
-         void Shoot()
-         {
-             muzzleFlash.Play();
-             RaycastHit hit;
-            if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
-            {
-                Debug.Log("shooting");
-            }
-            
-
-     }
-     
 
     }
+
+
+    void Equip(int p_ind)
+    {
+        if (currentWeapon != null) Destroy(currentWeapon);
+
+        currentIndex = p_ind;
+
+        GameObject t_newEquipment = Instantiate(loadout[p_ind].prefab, weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
+        t_newEquipment.transform.localEulerAngles = Vector3.zero;
+        t_newEquipment.transform.localPosition = Vector3.zero;
+
+        currentWeapon = t_newEquipment;
+    }
+
+    void Shoot()
+    {
+
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log("Shooting");
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(Damage);
+            }
+
+
+        }
+
+
+    }
+}
+    
